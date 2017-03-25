4f918c616be7350903396e8d3b34168a481a125a
Karl <karlanuetzel@gmail.com>
2017-03-25 15:39:53 -0500

    google api implemented

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var vision = require('@google-cloud/vision')({
+    projectId: 'picture-perfect-162617',
+    keyFilename: 'picture perfect-571e4afd6e07.json'
+});
 
 var mongoDB = 'mongodb://127.0.0.1:27017/local';
 mongoose.connect(mongoDB);
@@ -12,7 +16,8 @@ var schema = new mongoose.Schema({
     isJudge: Boolean,
     sourceId: String,
     imageId: String,
-    imageData: String
+    imageData: String,
+    tags: []
 });
 var model = mongoose.model(collectionName, schema);
 
@@ -23,11 +28,29 @@ app.set('port', process.env.PORT || 3000);
 app.post('/uploadPicture', function (req, res) {
     console.log("POST /uploadPicture received.");
 
+    var base64Data = req.body.imageData.replace(/^data:image\/jpeg;base64,/, "");
+
+    require("fs").writeFile("out.png", base64Data, 'base64', function(err) {
+        //console.log(err);
+    });
+
+    vision.detectLabels("out.png")
+        .then((results) => {
+            const labels = results[0];
+            console.log('Labels:');
+            labels.forEach((label) => {
+
+                console.log(label);
+
+            });
+        });
+
     var modelInstance = new model({
         isJudge: req.body.isJudge,
         sourceId: req.body.sourceId,
         imageId: req.body.imageId,
-        imageData: req.body.imageData
+        imageData: req.body.imageData,
+        tags: req.body.tags
     });
 
     modelInstance.save(function (err) {
@@ -49,6 +72,12 @@ app.post('/callGoogle', function (req, res) {
         } else {
             if (ret !== null) {
                 //ret contains the details of the image we want to send to google.
+
+
+
+
+
+
             } else {
                 console.log("No image associated with imageId \"" + req.body.imageId + "\", could not send any image to Google API.");
             }
