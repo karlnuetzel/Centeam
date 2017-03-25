73ce2a569e5a843d2c48233b2ee4020b8bc8911e
Justin <justinreusnow@gmail.com>
2017-03-25 14:58:45 -0500

    Stuff

--- index.js
+++ index.js
@@ -4,62 +4,61 @@ var mongoose = require("mongoose");
 
 var mongoDB = 'mongodb://127.0.0.1:27017/local';
 mongoose.connect(mongoDB);
+var collectionName = "perfectpicture";
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var Schema = mongoose.Schema;
-var ImageSchema = new Schema({
+var schema = new mongoose.Schema({
+    isJudge: Boolean,
+    sourceId: String,
     imageId: String,
     imageData: String
 });
-var ImageModel = mongoose.model('ImageModel', ImageSchema);
+var model = mongoose.model(collectionName, schema);
 
 var app = express();
+
+app.post('/uploadPicture', function (req, res) {
+    console.log("POST /uploadPicture received.");
+
+    console.log("REQ BODY: " + req.body);
+
+    var modelInstance = new model({
+        isJudge: req.body.isJudge,
+        sourceId: req.body.sourceId,
+        imageId: req.body.imageId,
+        imageData: req.body.imageData
+    });
+
+    modelInstance.save(function (err) {
+        if (err) {
+            return handleError(err);
+        }
+    });
+
+    res.send("Image with id \"" + req.body.imageId + "\" from user with id \"" + req.body.sourceId + "\" saved to Mongo.");
+});
+
+app.post('/callGoogle', function (req, res) {
+    console.log("POST /callGoogle received.");
+
+    var where = {'imageId': req.body.imageId};
+    model.findOne(where, function (err, ret) {
+        if (err) {
+            return handleError(err);
+        } else {
+            if (ret !== null) {
+                //ret contains the details of the image we want to send to google.
+            } else {
+                console.log("No image associated with imageId \"" + req.body.imageId + "\", could not send any image to Google API.");
+            }
+        }
+    });
+});
+
 app.use(bodyParser.json({limit: '50mb'}));
-
 app.set('port', process.env.PORT || 3000);
-
-var theImage = null;
-
-app.get('/', function (req, res) {
-    console.log('GET received');
-
-    res.send(theImage)
-});
-
-app.post('/upload', function (req, res) {
-    console.log("POST /upload received");
-    console.log(req.body.imageData);
-
-    var instance = new ImageModel({imageId: req.body.imageId, imageData: req.body.imageData});
-
-    instance.save(function (err) {
-        if (err) return handleError(err);
-    });
-
-    res.send(req.body);
-});
-
-app.post('/getImage', function (req, res) {
-    console.log("POST /getImage received");
-
-    console.log("req.boasdifjlsdkafj: " + req.body.imageId);
-
-    ImageModel.findOne({'imageId': req.body.imageId}, function (err, ret) {
-        console.log(ret);
-
-        var img = decodeImage(ret.imageData);
-
-
-    });
-});
-
-function decodeImage(imageData) {
-    var image = new Image();
-    image.src = imageData;
-    return image;
-}
-
 app.listen(app.get('port'));
+console.log("Listening on port " + app.get('port') + "...");
 
 module.exports = app;
