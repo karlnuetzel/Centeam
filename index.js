0cb48b104763b7f110e9ad2810d2d20106d3a40e
Justin <justinreusnow@gmail.com>
2017-03-25 14:06:21 -0500

    Right

--- index.js
+++ index.js
@@ -7,52 +7,15 @@ mongoose.connect(mongoDB);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
-//Define a schema
 var Schema = mongoose.Schema;
-
-var SomeModelSchema = new Schema({
-    picString: String,
-    gameID: String
+var ImageSchema = new Schema({
+    imageId: String,
+    imageData: String
 });
-
-var SomeModel = mongoose.model('SomeModel', SomeModelSchema);
-
-var awesome_instance = new SomeModel({gameID: 'fjk3l2rl'});
-
-awesome_instance.save(function (err) {
-    if (err) return handleError(err);
-});
-
-// mongoose.connect("mongodb://127.0.0.1:27017/local");
-
-// var db = mongoose.connection;
+var ImageModel = mongoose.model('ImageModel', ImageSchema);
 
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
-
-
-// var photoRouter = express.Router();
-// photoRouter.get("/", function(req, res) { });
-// photoRouter.post("/", postPhoto, function(req, res) { });
-// photoRouter.get("/:id", lookupPhoto, function(req, res) {
-//     res.json(req.photo);
-// });
-// photoRouter.patch("/:id", lookupPhoto, function(req, res) { });
-// photoRouter.delete("/:id", lookupPhoto, function(req, res) { });
-// app.use("/photo", photoRouter);
-//
-
-// function lookupPhoto(req, res, next) {
-//
-// }
-//
-// function postPhoto(req, res, next){
-//     //analyze photo with google API
-//     //save photo and analysis data in Mongo.
-//     //call function to send photo to other users
-//     //
-// }
 
 app.set('port', process.env.PORT || 3000);
 
@@ -65,11 +28,37 @@ app.get('/', function (req, res) {
 });
 
 app.post('/upload', function (req, res) {
-    console.log("POST received");
-    console.log(req.body);
+    console.log("POST /upload received");
+    console.log(req.body.imageData);
+
+    var instance = new ImageModel({imageId: req.body.imageId, imageData: req.body.imageData});
+
+    instance.save(function (err) {
+        if (err) return handleError(err);
+    });
 
     res.send(req.body);
 });
+
+app.post('/getImage', function (req, res) {
+    console.log("POST /getImage received");
+
+    console.log("req.boasdifjlsdkafj: " + req.body.imageId);
+
+    ImageModel.findOne({'imageId': req.body.imageId}, function (err, ret) {
+        console.log(ret);
+
+        var img = decodeImage(ret.imageData);
+
+
+    });
+});
+
+function decodeImage(imageData) {
+    var image = new Image();
+    image.src = imageData;
+    return image;
+}
 
 app.listen(app.get('port'));
 
